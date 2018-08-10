b2df5ee5af63d2a59b390872dd8ac11203711384
Justin Landis <jtlandis@live.unc.edu>
2018-08-10 12:16:52 -0400

    adding updating man files

--- R/initialization.R
+++ R/initialization.R
@@ -1,5 +1,5 @@
 #' @title FindHillBounds
-#' Compute the Hill bounds based on initial parameter estimates and data.
+#' @description Compute the Hill bounds based on initial parameter estimates and data.
 #' @name FindHillBounds
 #' 
 #' @param x Vector of doses.
@@ -115,7 +115,8 @@ FindHillBounds <- function(x, y, theta,
   return(Hill.bounds)
 }
 
-#' Compute the grids on the upper and lower asymptote parameters for the logistic
+#' @title FindLogisticGrids
+#' @description Compute the grids on the upper and lower asymptote parameters for the logistic
 #' method based on initial parameter estimates and data.
 #'
 #' @name FindLogisticGrids
@@ -233,7 +234,8 @@ FindLogisticGrids <- function(x, y, retheta.init, use.Hessian = FALSE) {
   return(logistic.grids)
 }
 
-#' Find initial parameter estimates for a 4PL model.
+#' @title FindInitialParms
+#' @description Find initial parameter estimates for a 4PL model.
 #'
 #' @name FindInitialParms
 #'
